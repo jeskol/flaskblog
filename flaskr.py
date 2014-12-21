413b8674e11b7c606cbcdce1ccccc610fbbaf2d8
Jesse K <krumble@usian.org>
2014-12-21 13:46:47 -0500

    External config file and init_db added

--- flaskr.py
+++ flaskr.py
@@ -2,10 +2,11 @@
 
 # all the imports
 import sqlite3
+from contextlib import closing
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
-# configuration
-DATABASE = '/home/krumble/work/personal/flaskr/flaskr.db'
+# default configuration
+DATABASE = 'flaskr.db'
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
@@ -15,7 +16,13 @@ PASSWORD = 'default'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-#app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+app.config.from_envvar('FLASKR_SETTINGS', silent=False)
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
